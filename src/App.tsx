a21d3f6673ed003a66e13c52fceb44f6f1259ba2
VadimDgahm <dgahm@mail.ru>
2023-12-03 10:38:45 +0300

    instal

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,17 @@
+<<<<<<< HEAD
 import { DropDownMenu } from '@/components/ui/dropDownMenu/DropDownMenu'
 
 function App() {
+=======
+import { useState } from 'react'
+
+import { DropDownMenu } from '@/components/ui/dropDownMenu/DropDownMenu'
+
+function App() {
+  const [values, setValues] = useState([0, 20])
+  const sliderCallback = (newValues: number[]) => setValues(newValues)
+
+>>>>>>> origin/main
   return (
     <div style={{ margin: '0 auto', padding: '100px', width: '200px' }}>
       <DropDownMenu>
